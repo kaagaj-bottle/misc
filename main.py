877f431945b8fc132225324cc1de4c0dd859234f
Zishan Siddique <zishansiddique80@gmail.com>
2024-06-03 22:34:29 +0545

    new test for dict type added

--- main.py
+++ main.py
@@ -47,6 +47,21 @@ rules = {
     "location": {"type": "list", "length": {"min": 2, "max": 4}, "itemType": "str"},
     "hobbies": {"type": "list", "length": {"min": 1}, "itemType": "int"},
     "isWorkingYet": {"type": "bool", "value": True},
+    "dictValue": {
+        "type": "dict",
+        "value": {
+            "age": {"type": "int", "min": 18, "max": 40},
+            "salary": {"type": "float", "min": 10000, "max": 50000},
+            "name": {"type": "str", "min": 10, "max": 25},
+            "location": {
+                "type": "list",
+                "length": {"min": 2, "max": 4},
+                "itemtype": "str",
+            },
+            "hobbies": {"type": "list", "length": {"min": 1}, "itemType": "int"},
+            "isWorkingYet": {"type": "bool", "value": True},
+        },
+    },
 }
 
 data = {
@@ -56,4 +71,12 @@ data = {
     "location": ["Kathmandu", "Nepal"],
     "hobbies": [1, 2, 3],
     "isWorkingYet": False,
+    "dictValue": {
+        "age": 23,
+        "salary": 30000.0,
+        "name": "Zishan Siddique",
+        "location": ["Kathmandu", "Nepal"],
+        "hobbies": [1, 2, 3],
+        "isWorkingYet": False,
+    },
 }
